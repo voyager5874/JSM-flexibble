1748a407a4e9c2d08216178144e9427a1488713f
Aleksandr Savkin <voyager5874@gmail.com>
2023-06-26 21:54:00 +0300

    create basic layout and data models

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import { NavLinks } from "@/constant";
+
+export const Navbar = async () => {
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={116} height={43} alt="logo" />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.text}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <button>sign in</button>
+    </nav>
+  );
+};
